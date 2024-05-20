32f3c662cb343104d5db42934b11442d3fceea69
ulrichgero <49178952+ulrichgero@users.noreply.github.com>
2024-05-20 19:36:13 +0100

    update file format

--- Step.sql
+++ Step.sql
@@ -1,27 +1,24 @@
-**Duplicate Table**
+-- Duplicate Table
 
 
-``` sql
+
 CREATE TABLE data_2022
 LIKE FAOSTAT_DATA_2022
-```
+
 
 -- check if every columns has been duplicated correctly
 
-```sql
+
 SELECT * 
 FROM data_2022 
-```
+
 
 -- Now let copy everything from orginal table to the copy
 
-```sql
+
 INSERT data_2022 
 SELECT *
 FROM FAOSTAT_DATA_2022 
-```
 
+-- Let do the same with data from 2020
 
-```sql  
-
-```
